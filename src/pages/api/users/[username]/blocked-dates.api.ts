5166e3b66969000b03cb5d8c20c86b49661d157f
vinicius <vinicius.souza.assis@gmail.com>
2024-12-26 00:02:28 -0300

    queries sql in prisma whith queryRaw

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -57,6 +57,8 @@ export default async function handle(
 
 	GROUP BY EXTRACT(DAY FROM S.date),
 		((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+	HAVING amount >= size
 	`;
 
 	console.log(blockedDatesRaw);
